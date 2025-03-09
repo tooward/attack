adbfa5ed788cce8673eaade5e060dd22c0a8fdd5
Mike IOG <mikeiog@m4ward.local>
2025-03-09 20:46:34 +0000

    Initial commit

---
+++ src.js
@@ -0,0 +1,85 @@
+var config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300 },
+            debug: false
+        }
+    },
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+var player;
+var platforms;
+var cursors;
+var game = new Phaser.Game(config);
+
+function preload ()
+{
+    this.load.image('ground', 'assets/platform.png');
+    this.load.spritesheet('dude-run', 'assets/_Run.png', { frameWidth: 120, frameHeight: 80 });
+    this.load.spritesheet('dude-idle', 'assets/_Idle.png', { frameWidth: 120, frameHeight: 80 });
+}
+
+function create () {
+
+   platforms = this.physics.add.staticGroup();
+   platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+
+    // Create the player sprite (choose the appropriate texture)
+    player = this.physics.add.sprite(300, 350, 'dude-idle');
+    player.setCollideWorldBounds(true);
+
+    this.anims.create({
+        key: 'left',
+        frames: this.anims.generateFrameNumbers('dude-run', { start: 0, end: 9 }),
+        frameRate: 10,
+        repeat: 1
+    });
+
+    this.anims.create({
+        key: 'right',
+        frames: this.anims.generateFrameNumbers('dude-run', { start: 0, end: 9 }),
+        frameRate: 10,
+        repeat: -1
+    });
+
+    this.anims.create({
+        key: 'idle',
+        frames: this.anims.generateFrameNumbers('dude-idle', { start: 0, end: 9 }),
+        frameRate: 10
+    });
+
+    cursors = this.input.keyboard.createCursorKeys();
+    this.physics.add.collider(player, platforms);
+//    player.anims.play('idle', true);
+}
+
+function update ()
+{
+    if (cursors.left.isDown)
+    {
+        player.setVelocityX(-160);
+
+        player.anims.play('left', true);
+    }
+    else if (cursors.right.isDown)
+    {
+        player.setVelocityX(160);
+
+        player.anims.play('right', true);
+    }
+    else
+    {
+        player.setVelocityX(0);
+
+        player.anims.play('idle', true);
+    }
+}
